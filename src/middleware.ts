b361056299ac93990c52df57d766647e0b0f285e
Ramazan <ozturkiramazan@gmail.com>
2024-02-20 00:40:20 +0300

    added prisma binaryTargets to fix deployment issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,7 @@ import {authMiddleware} from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/list/(.*)", "/api/list/delete/(.*)", "/api/webhooks/clerk"],
   apiRoutes: ["/api/list/update/(.*)", "/api/list/create"],
-  ignoredRoutes: [
-    "/googled47fba71fc25af17.html",
-    "/assets/(.*)",
-    "/api/list/get/(.*)",
-    "/api/list/getAll",
-    "/api/tmdb/(.*)",
-  ],
+  ignoredRoutes: ["/assets/(.*)", "/api/list/get/(.*)", "/api/list/getAll", "/api/tmdb/(.*)"],
 });
 
 export const config = {
