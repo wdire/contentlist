52e09b16492d07d180d2d8835bd1b2af55b3b06f
Ramazan <ozturkiramazan@gmail.com>
2024-02-27 21:11:35 +0300

    fix: prisma dev env prevent dublicate instances

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,13 @@ const prismaClientSingleton = () => {
   return new PrismaClient({log: ["error", "info", "warn"]});
 };
 
-const prisma = prismaClientSingleton();
+declare global {
+  // eslint-disable-next-line vars-on-top, no-var
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
